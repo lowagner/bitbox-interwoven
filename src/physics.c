9c6292fd0ce3468d100098ae60290321a522956b
Lucas Wagner <lowagner@gmail.com>
2021-04-12 14:02:15 -0700

    simplify physics engine
    don't do impulses for dynamic <-> dynamic object interaction, that's asking for hardship

--- src/physics.c
+++ src/physics.c
@@ -3,6 +3,50 @@
 
 physics_object_t physics_object[MAX_PHYSICS_OBJECTS] CCM_MEMORY;
 physics_collision_t physics_collision[MAX_PHYSICS_COLLISIONS] CCM_MEMORY;
+struct physics_static physics_static CCM_MEMORY;
+
+static inline void min_vectors(float *min, const float *va, const float *vb)
+{   min[0] = va[0] < vb[0] ? va[0] : vb[0];
+    min[1] = va[1] < vb[1] ? va[1] : vb[1];
+    min[2] = va[2] < vb[2] ? va[2] : vb[2];
+}
+
+static inline void max_vectors(float *max, const float *va, const float *vb)
+{   max[0] = va[0] > vb[0] ? va[0] : vb[0];
+    max[1] = va[1] > vb[1] ? va[1] : vb[1];
+    max[2] = va[2] > vb[2] ? va[2] : vb[2];
+}
+
+static inline void boundary_union(physics_boundary_t *dst, const physics_boundary_t *b1, const physics_boundary_t *b2)
+{   min_vectors(dst->corner_min, b1->corner_min, b2->corner_min);
+    max_vectors(dst->corner_max, b1->corner_max, b2->corner_max);
+}
+
+static inline void average_vectors(float *avg, const float *va, const float *vb)
+{   avg[0] = 0.5f * (va[0] + vb[0]);
+    avg[1] = 0.5f * (va[1] + vb[1]);
+    avg[2] = 0.5f * (va[2] + vb[2]);
+}
+
+static inline void diff_vectors(float *diff, const float *va, const float *vb)
+{   diff[0] = va[0] - vb[0];
+    diff[1] = va[1] - vb[1];
+    diff[2] = va[2] - vb[2];
+}
+
+void physics_static_move(int index, physics_boundary_t new_boundary)
+{   // Move a static object around.  Computes the velocity and new size from the new_boundary as well.
+    ASSERT(index >= 0 && index < physics_static.count);
+    physics_entity_t *entity = &physics_static.entity[index];
+    float new_center[3];
+    float old_center[3];
+    average_vectors(new_center, new_boundary.corner_min, new_boundary.corner_max);
+    average_vectors(old_center, entity->boundary.corner_min, entity->boundary.corner_max);
+    diff_vectors(entity->size, new_boundary.corner_max, new_boundary.corner_min);
+    diff_vectors(entity->velocity, new_center, old_center);
+    boundary_union(&entity->frame_movement, &new_boundary, &entity->boundary);
+    memcpy(&entity->boundary, &new_boundary, sizeof(physics_boundary_t));
+}
 
 void physics_reset()
 {   // Resets physics engine
@@ -23,7 +67,7 @@ static inline void physics_reset_collisions()
 {   LL_RESET(physics_collision, next_collision, previous_collision, MAX_PHYSICS_COLLISIONS); 
     uint8_t object = physics_object[0].next_object;
     while (object)
-    {   memset(physics_object[object].collisions, 0, 6);
+    {   physics_object[object].all_collisions = 0;
         object = physics_object[object].next_object;
     }
 }
@@ -52,6 +96,8 @@ int physics_overlap(const physics_boundary_t *b1, const physics_boundary_t *b2)
 #ifdef EMULATOR
 void test_physics()
 {   // Physics tests to make sure everything is working correctly.
+    // probably need at least one collision per object, e.g., for standing on a surface
+    ASSERT(MAX_PHYSICS_COLLISIONS >= MAX_PHYSICS_OBJECTS * 2);
     {   // Test boundaries overlapping in x only
         physics_boundary_t boundary1 = {
             .corner_min = {0, -1, 1},
@@ -139,6 +185,23 @@ void test_physics()
             .corner_max = {2, 0, 4},
         };
         ASSERT(physics_overlap(&boundary1, &boundary2) == 1);
+    }
+    {   // Test union boundaries
+        physics_boundary_t boundary1 = {
+            .corner_min = {-5, -1, 1},
+            .corner_max = {-2, 10, 4},
+        };
+        physics_boundary_t boundary2 = {
+            .corner_min = {-6, 0, 1},
+            .corner_max = {-1, 3, 5},
+        };
+        physics_boundary_t expected = {
+            .corner_min = {-6, -1, 1},
+            .corner_max = {-1, 10, 5},
+        };
+        physics_boundary_t value;
+        boundary_union(&value, &boundary1, &boundary2);
+        ASSERT(memcmp(&value, &expected, sizeof(physics_boundary_t)) == 0);
     }
     message("physics tests passed!\n");
 }
--- src/physics.h
+++ src/physics.h
@@ -3,44 +3,65 @@
 
 #include <stddef.h>
 
+#define MAX_PHYSICS_STATICS 16
 #define MAX_PHYSICS_OBJECTS 16
-#define MAX_PHYSICS_COLLISIONS 64
+#define MAX_PHYSICS_COLLISIONS 32
 
 typedef struct physics_boundary
 {   // A rectangular prism representing a physics boundary which should not be crossed.
     float corner_min[3], corner_max[3];
 } physics_boundary_t;
 
+typedef struct physics_entity
+{   // If a static entity, it can be moved around programatically using the `physics_static_move` function,
+    // or if inside a physics_object you can just update the velocity.
+    // Current boundary of the entity:
+    physics_boundary_t boundary;
+    // Size in each dimension x,y,z, e.g. width/length/height:
+    float size[3];
+    // TODO: add support for this:
+    // Things that are on top of this entity will experience a force in the direction of velocity due to friction
+    float velocity[3];
+    // Min/max boundaries for the movement that we just did in the last frame:
+    physics_boundary_t frame_movement;
+} physics_entity_t;
+
 typedef struct physics_object
 {   // Representation of an object that has dynamical physics applied.
     // Linked list representation:
     uint8_t next_object;
     uint8_t previous_object;
-    // Indices to collisions in the last frame, or zero.
     union
     {   uint8_t next_free; // used in root physics_object only.
-        uint8_t collisions[6];
+        // Indices to collisions in the last frame, or zero.
+        uint8_t collision[4];
+        uint32_t all_collisions;
     };
-    // 
-    physics_boundary_t boundary;
-    // Size in each dimension x,y,z, e.g. width/length/height:
-    float size[3];
-    float velocity[3];
-    // Min/max boundaries for the movement that we just did in the last frame:
-    physics_boundary_t frame_movement;
+    physics_entity_t entity;
 } physics_object_t;
 
 extern physics_object_t physics_object[MAX_PHYSICS_OBJECTS];
 
+struct physics_static
+{   // Rectangular prisms that only interact with dynamic `physics_object`s but does not react to any forces.
+    physics_entity_t entity[MAX_PHYSICS_STATICS];
+    // Number of static entities:
+    uint8_t count;
+};
+
+extern struct physics_static physics_static;
+
 typedef struct physics_collision
 {   // Description of a collision
     uint8_t next_collision, previous_collision;
-    // These indices are 1-15 for a dynamic object, 17-31 for static terrain.
-    // index1 should always be from 1-15, since only dynamic objects care about collisions.
-    uint8_t index1, index2;
-   
+    // These indices are 1 to (MAX_PHYSICS_OBJECTS-1) for a dynamic object, or
+    // MAX_PHYSICS_OBJECTS to (MAX_PHYSICS_OBJECTS+MAX_PHYSICS_STATICS-1) for static terrain.
+    // Note: index1 should always be from 1 to (MAX_PHYSICS_OBJECTS-1),
+    // since only dynamic objects care about collisions.
+    uint8_t index1;
     union {
-        float impulse[3]; // impulse as felt by object at index1; index2 feels the opposite.
+        uint8_t index2;
+        // only used in root collision:
         uint8_t next_free;
     };
 } physics_collision_t;
@@ -48,6 +69,7 @@ typedef struct physics_collision
 extern physics_collision_t physics_collision[MAX_PHYSICS_COLLISIONS];
 
 void physics_reset();
+void physics_static_move(int index, physics_boundary_t new_boundary);
 uint8_t physics_new_object();
 void physics_free_object(uint8_t index);
 void physics_frame();
